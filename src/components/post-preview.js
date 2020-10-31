87ee95f552b13d2af2db9f6c0dfbcf0365a9b560
coskun cakir <csknckr@gmail.com>
2020-10-31 13:18:29 +0000

    initial

---
+++ src/components/post-preview.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default function PostPreview({ post }) {
+  return (
+    <article
+      style={{ padding: "1rem 2rem", background: "#eee", marginBottom: "1rem" }}
+    >
+      <h3>{post.title}</h3>
+      <p>{post.excerpt}</p>
+      <Link to={post.slug}>read more &rarr;</Link>
+    </article>
+  )
+}
---
+++ src/hooks/use-post.js
@@ -0,0 +1,23 @@
+import { graphql, useStaticQuery } from "gatsby"
+
+const usePosts = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMdx {
+        nodes {
+          frontmatter {
+            title
+          }
+          excerpt
+        }
+      }
+    }
+  `)
+
+  return data.allMdx.nodes.map(post => ({
+    title: post.frontmatter.title,
+    excerpt: post.excerpt,
+  }))
+}
+
+export default usePosts
